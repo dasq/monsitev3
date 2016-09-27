93a9ee7bd4abb7746596e3bdaac4360faab2f928
dasq <darifarid@gmail.com>
2016-09-27 23:11:25 +0200

    SC & YT iframe delayed

--- js/one.js
+++ js/one.js
@@ -275,3 +275,68 @@ jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
 		$(document).ready(function(){
 			$("#menu-container").sticky({topSpacing:0});
 		});
+		
+ 
+    /* Light YouTube Embeds by @labnol */
+    /* Web: http://labnol.org/?p=27941 */
+ 
+    document.addEventListener("DOMContentLoaded",
+        function() {
+            var div, n,
+                v = document.getElementsByClassName("youtube-player");
+            for (n = 0; n < v.length; n++) {
+                div = document.createElement("div");
+                div.setAttribute("data-id", v[n].dataset.id);
+                div.innerHTML = labnolThumb(v[n].dataset.id);
+                div.onclick = labnolIframe;
+                v[n].appendChild(div);
+            }
+        });
+ 
+    function labnolThumb(id) {
+        var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
+            play = '<div class="play"></div>';
+        return thumb.replace("ID", id) + play;
+    }
+ 
+    function labnolIframe() {
+        var iframe = document.createElement("iframe");
+        var embed = "https://www.youtube.com/embed/ID?autoplay=1";
+        iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+        iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("allowfullscreen", "1");
+        this.parentNode.replaceChild(iframe, this);
+    }
+ 
+    /* Light Soundcloud Embeds by @labnol */
+    /* Web: http://labnol.org/?p=27941 */
+ 
+    document.addEventListener("DOMContentLoaded",
+        function() {
+            var div, n,
+                v = document.getElementsByClassName("soundcloud-player");
+            for (n = 0; n < v.length; n++) {
+                div = document.createElement("div");
+                div.setAttribute("data-id", v[n].dataset.id);
+                div.innerHTML = labnolThumbSC(v[n].dataset.id);
+                div.onclick = labnolIframeSC;
+                v[n].appendChild(div);
+            }
+        });
+	
+	function labnolThumbSC(id) {
+		var thumb = '<img src="img/ID-sc.png">',
+            play = '<div class="play"></div>';        
+        return thumb.replace("ID", id) + play;
+    }
+
+ 
+    function labnolIframeSC() {
+        var iframe = document.createElement("iframe");
+        var embed = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/ID&amp;show_user=true&amp;visual=true&amp;auto_play=true";
+        iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+        iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("allowfullscreen", "1");
+        this.parentNode.replaceChild(iframe, this);
+    }
+ 
